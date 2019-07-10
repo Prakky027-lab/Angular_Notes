3ace700c72d8f325b387bfdab69b4827e8de9b82
Prakhar Khetan <51966080+Prakky027-lab@users.noreply.github.com>
2019-07-10 17:33:16 +0530

    Add files via upload

---
+++ Exercise3/app/app.addproduct.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+//import { EmployeeService } from "./app.employeeservice";
+import { Router } from "@angular/router";
+
+
+@Component({
+    selector:'add-comp',
+    templateUrl:'addproduct.html'
+})
+export class AddProductComponent{
+    id:any;
+    nmae:any;
+    cost:any;
+}
